d6433173adee48ad04bc88211a2cfb4817a3f30b
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-30 20:42:49 +0300

    feat: header wiyhout swiper buttons

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  var mySwiper = new Swiper ('.hero__swiper', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+})
